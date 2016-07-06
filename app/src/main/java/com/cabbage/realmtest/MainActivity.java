bf303abe39e0c5e486267de8d1f5edcacae1b221
DukeCabbage <liaowng@gmail.com>
2016-07-05 18:19:48 -0700

    sfd

--- app/src/main/java/com/cabbage/realmtest/MainActivity.java
+++ app/src/main/java/com/cabbage/realmtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cabbage.realmtest;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,15 +10,21 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmList;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,7 +39,14 @@ public class MainActivity extends AppCompatActivity {
         String input = etPersonName.getText().toString();
         if (!TextUtils.isEmpty(input)) {
             Person newPerson = new Person();
-            newPerson.setFirstName(input);
+            Random rnd = new Random();
+            newPerson.setFirstName(input + rnd.nextInt(100));
+
+            Realm realm = Realm.getDefaultInstance();
+            realm.beginTransaction();
+            realm.insert(newPerson);
+            realm.commitTransaction();
+
             mAdatper.addPerson(newPerson);
         }
     }
@@ -56,17 +70,47 @@ public class MainActivity extends AppCompatActivity {
 
         rvPersons.setHasFixedSize(false);
         rvPersons.setLayoutManager(new LinearLayoutManager(this));
+        rvPersons.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST, 0, false, true));
         mAdatper = new PersonAdapter();
         rvPersons.setAdapter(mAdatper);
+
+        rvPersons.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                    View view = MainActivity.this.getCurrentFocus();
+                    if (view != null) {
+                        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    }
+                }
+            }
+        });
+
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(realmConfig);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        RealmQuery<Person> query = Realm.getDefaultInstance().where(Person.class);
+        RealmResults<Person> results = query.findAll();
+        for (Person result : results) {
+            mAdatper.addPerson(result);
+        }
     }
 
     public static class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder> {
 
-        private List<Person> mData = new ArrayList<>();
+        private List<Person> mData = new RealmList<>();
 
         public void addPerson(Person person) {
-            mData.add(person);
-            notifyItemChanged(getItemCount() - 1);
+            if (!mData.contains(person)) {
+                mData.add(0, person);
+                notifyDataSetChanged();
+            }
         }
 
         @Override
